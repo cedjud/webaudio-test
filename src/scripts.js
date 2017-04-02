f41085ebdb59ddb39d8d327ac5fcd25061faf804
cedjud <cedricjud@outlook.com>
2017-04-03 00:10:33 +0200

    setup basic grid.

--- src/scripts.js
+++ src/scripts.js
@@ -1,96 +1,119 @@
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+function createAudio(){
+  audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-var oscillator = audioCtx.createOscillator();
-var gainNode = audioCtx.createGain();
+  oscillator = this.audioCtx.createOscillator();
+  gainNode = audioCtx.createGain();
 
-var WIDTH = window.innerWidth;
-var HEIGHT = window.innerHeight;
+  oscillator.type = 'sine';
+  oscillator.frequency.value = 300;
 
-var maxFreq = 600;
-var maxVol = 1;
+  gainNode.gain.value = 0.5;
 
-var initialFreq = 300;
-var initialVol = 0.5;
+  oscillator.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+  oscillator.start();
+}
 
-// set options for the oscillator
-oscillator.type = 'sine'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
-oscillator.frequency.value = initialFreq; // value in hertz
-// oscillator.start();
+function stopAudio(){
+  oscillator.stop();
+}
 
-gainNode.gain.value = initialVol;
+var buttonClick = function(id, osc){
+  var editedId = (id * 10) + 100;
 
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-
-// // Mouse pointer coordinates
-// var CurX;
-// var CurY;
-//
-// // Get new mouse pointer coordinates when mouse is moved
-// // then set new gain and pitch values
-//
-// document.onmousemove = updatePage;
-//
-// function updatePage(e) {
-//     CurX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-//     CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-//
-//     oscillator.frequency.value = (CurX/WIDTH) * maxFreq;
-//     gainNode.gain.value = (CurY/HEIGHT) * maxVol;
-//
-//      canvasDraw();
-// }
-//
-// function random(number1,number2) {
-//   var randomNo = number1 + (Math.floor(Math.random() * (number2 - number1)) + 1);
-//   return randomNo;
-// }
-//
-// var canvas = document.querySelector('.canvas');
-// canvas.width = WIDTH;
-// canvas.height = HEIGHT;
-//
-// var canvasCtx = canvas.getContext('2d');
-//
-// function canvasDraw() {
-//   rX = CurX;
-//   rY = CurY;
-//   rC = Math.floor((gainNode.gain.value/maxVol)*30);
-//
-//   canvasCtx.globalAlpha = 0.2;
-//
-//   for(i=1;i<=15;i=i+2) {
-//     canvasCtx.beginPath();
-//     canvasCtx.fillStyle = 'rgb(' + 100+(i*10) + ',' + Math.floor((gainNode.gain.value/maxVol)*255) + ',' + Math.floor((oscillator.frequency.value/maxFreq)*255) + ')';
-//     canvasCtx.arc(rX+random(0,50),rY+random(0,50),rC/2+i,(Math.PI/180)*0,(Math.PI/180)*360,false);
-//     canvasCtx.fill();
-//     canvasCtx.closePath();
-//   }
-// }
-
-var toggle = function(action){
-  switch (action) {
-    case 'start':
-        console.log('start');
-        oscillator.start();
-      break;
-    case 'stop':
-        console.log('stop');
-        oscillator.stop();
-      break;
-    default:
-      var value = parseInt(action + '00');
-      oscillator.frequency.value = value;
-      console.log(typeof(value));
+  try {
+    oscillator.frequency.value = editedId;
+  } catch (e) {
+    console.log(e)
   }
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-  var buttons = document.querySelectorAll('button');
+// create an individual button
+var createString = function(id){
+  var stringStart = "<div class='item' onclick='buttonClick(" + id + ")' id='item-" + id + "'>"
+  var stringEnd = "</div>";
+  var stringButton1 = "<button id='start'>start</button>";
 
-  for (i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener('click', function(){
-      toggle(this.id);
-    });
+  return stringStart + stringButton1 + stringEnd;
+};
+
+// create a row of buttons
+var createRow = function(id, width){
+  var stringStart = "<div class='row' id='row-" + id + "'>";
+  var stringEnd = "</div>";
+  var string = "";
+
+  for (var i = 0; i < width; i++){
+    var buttonId = id + "" + i;
+    var button = createString(buttonId);
+    string += button;
   }
+  return stringStart + string + stringEnd;
+}
+
+// create a grid by creating rows of buttons
+var createGrid = function(countX, countY){
+  var grid = '';
+  for (var j = 0; j < countY; j++ ){
+    grid += createRow(j, countX);
+  }
+  return grid;
+}
+
+var getRandomCoordinate = function(){
+  var num = Math.floor(Math.random()* (125 - 50) + 75); // this will get a number between 1 and 99;
+  num *= Math.floor(Math.random()*2) == 1 ? 1 : -1; // this will add minus sign in 50% of cases
+  return num;
+}
+
+
+// GSAP Animation functions
+var animateDotsIn = function(items){
+  for (var i = 0; i < items.length; i++){
+    var x = getRandomCoordinate();
+    var y = getRandomCoordinate();
+    TweenMax.set(items[i], {z:0.01});
+    TweenMax.from(items[i], 6, {opacity: 0, delay: 2});
+    TweenMax.from(items[i], 5, {x: x, y: y, ease: Quad.easeInOut});
+  }
+};
+
+var animateMiddleDots = function(dots){
+  TweenMax.set(dots, {opacity: 0});
+  TweenMax.to(dots, .2, {opacity: 1, delay: 10.2});
+}
+
+var animateTextIn = function(){
+  TweenMax.from('#title', 1, {opacity: 0, delay: 3});
+  TweenMax.to('#title', .2, {opacity: 0, delay: 10});
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  var container = document.getElementById('container');
+
+  var countX = 10;
+  var countY = 9;
+
+  var grid = createGrid(countX, countY);
+
+  container.innerHTML += grid;
+
+  var items = document.querySelectorAll('.item');
+
+  var hiddenDots = [];
+  var visibleDots = [];
+
+  items.forEach(function(item, index){
+    if ( (index > 31 && index < 38) || (index > 41 && index < 48) || (index > 51 && index < 58) ){
+      hiddenDots.push(item);
+    } else {
+      visibleDots.push(item);
+    }
+  })
+
+  animateDotsIn(visibleDots);
+  animateMiddleDots(hiddenDots);
+  animateTextIn(text);
+
+  TweenMax.from('.btn', 1, {opacity: 0, delay: 3});
 });
